614d3bd5b852515034e5e8dcbcc59fab6f9a1e20
Pedro Santana <pedroschw.c@gmail.com>
2023-06-26 09:50:17 -0300

    using card from db.json

--- React_1/src/pages/Inicio/index.js
+++ React_1/src/pages/Inicio/index.js
@@ -1,21 +1,27 @@
+import styles from "./Inicio.module.css";
 import Titulo from "components/Titulo";
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Card from "components/Card";
+import videos from "json/db.json";
 
 function Inicio() {
-    return (
-        <>
-            <Cabecalho />
-            <Banner imagem="home" />
-            <Titulo>
-                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
-            </Titulo>
-            <Card id="1" titulo="Filme 1" capa="https://picsum.photos/200/300" />
-            <Rodape />
-        </>
-    )
+  return (
+    <>
+      <Cabecalho />
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
+      <Rodape />
+    </>
+  );
 }
 
 export default Inicio;
