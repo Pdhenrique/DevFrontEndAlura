408d915e9ad5e654acad3566602e67902adf439f
Pedro Santana <pedroschw.c@gmail.com>
2023-06-28 13:04:15 -0300

    feat: created banner component

---
+++ React_2/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export default function Banner(){
+    return(
+        <header className="banner">
+            <img alt="img1" src="/images/banner.png"/>
+        </header>
+    )
+}
+
