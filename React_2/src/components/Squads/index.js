b64acbdd591c34199dbcf04bc9b325138c7acf28
Pedro Santana <pedroschw.c@gmail.com>
2023-06-29 14:25:35 -0300

    refactor: modified component structure

--- React_2/src/components/Squads/index.js
+++ React_2/src/components/Squads/index.js
@@ -1,19 +1,27 @@
+import Collaborator from "../Collaborator";
 import "./Squads.css";
 
 const Squads = (props) => {
   return (
-    <section
-      className="squad"
-      style={{ backgroundColor: props.secondaryColor }}
-    >
-      <h3
-        style={{
-          borderColor: props.primaryColor,
-        }}
+    props.collaborators.length > 0 && (
+      <section
+        className="squad"
+        style={{ backgroundColor: props.secondaryColor }}
       >
-        {props.name}
-      </h3>
-    </section>
+        <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+
+        <div className="collaborators">
+          {props.collaborators.map((collaborator) => (
+            <Collaborator
+              key={collaborator.name}
+              name={collaborator.name}
+              role={collaborator.role}
+              image={collaborator.image}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
