854ec7eeb20eb0be343a38a30d940099da5983cb
Pedro Santana <pedroschw.c@gmail.com>
2023-06-29 13:30:59 -0300

    refactor: added squads props list

--- React_2/src/App.js
+++ React_2/src/App.js
@@ -1,15 +1,65 @@
+import { useState } from "react";
 import "./App.css";
-import Banner from "./components/Banner/index.js";
+import Banner from "./components/Banner";
 import Form from "./components/Form";
-
+import Squads from "./components/Squads";
 
 const App = () => {
+  const squads = [
+    {
+      name: "Front-End",
+      primaryColor: "#82CFFA",
+      secondaryColor: "#E8F8FF",
+    },
+    {
+      name: "Data Sciense",
+      primaryColor: "#A6D157",
+      secondaryColor: "#F0F8E2",
+    },
+    {
+      name: "Devops",
+      primaryColor: "#E06B69",
+      secondaryColor: "#FDE7E8",
+    },
+    {
+      name: "UX e Design",
+      primaryColor: "#D86EBF",
+      secondaryColor: "#FAE5F5",
+    },
+    {
+      name: "Mobile",
+      primaryColor: "#FEBA05",
+      secondaryColor: "#FFF5D9",
+    },
+    {
+      name: "Inovação e Gestão",
+      primaryColor: "#FF8A29",
+      secondaryColor: "#FFEEDF",
+    },
+  ];
+
+  const [collaborators, setCollaborators] = useState([]);
+
+  const newCollaborator = (collaborator) => {
+    setCollaborators([...collaborators, collaborator]);
+  };
+
   return (
     <div className="app">
       <Banner />
-      <Form/>
+      <Form squads={squads.map(squad => squad.name)}
+        beRegisterCollaborator={(collaborator) => newCollaborator(collaborator)}
+      />
+      {squads.map((squad) => (
+        <Squads
+          key={squad.name}
+          name={squad.name}
+          primaryColor={squad.primaryColor}
+          secondaryColor={squad.secondaryColor}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
