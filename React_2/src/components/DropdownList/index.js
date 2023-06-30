a5876767ee10fafe5ba467b96e3545b1665abb37
Pedro Santana <pedroschw.c@gmail.com>
2023-06-30 09:38:53 -0300

    refactor: changed behavior of select

--- React_2/src/components/DropdownList/index.js
+++ React_2/src/components/DropdownList/index.js
@@ -9,6 +9,7 @@ const DropdownList = (props) => {
         required={props.required}
         value={props.value}
       >
+        <option value=""></option>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
