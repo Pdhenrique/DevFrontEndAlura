8c79dfa901ffa403bc4cad33794c517da6b50342
Pedro Santana <pedroschw.c@gmail.com>
2023-07-03 12:43:20 -0300

    style: added dark theme stylization

--- ReactStyledComp/src/Components/UI/variables.js
+++ ReactStyledComp/src/Components/UI/variables.js
@@ -1,5 +1,10 @@
 export const primaryColor = "#41b3b3";
 
-export const backgroundLight = "#f1f1f1";
+export const lightTheme = "#f1f1f1";
+export const lightContent = "white"
+export const lightThemeText = "grey";
 
-export const backgroundLightText = "grey";
+
+export const darkTheme = "#363537"
+export const darkContent = "#5c5b5e"
+export const darkThemeText = "#FAFAFA"
