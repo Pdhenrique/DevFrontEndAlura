35c261421fc3d9d3242de265e0f94c8403b70bce
Pedro Santana <pedroschw.c@gmail.com>
2023-07-07 12:47:27 -0300

    refactor: removed unused files

--- ByteBankReact/src/components/Header/Header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './index';
-
-test('should render user name', () => {
-  render(<Header />);
-  const userName = screen.getByText('Joana Fonseca Gomes');
-  expect(userName).toBeInTheDocument();
-});
--- ByteBankReact/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const calculateNewBalance = (values, balance) => {
-  if (values.transaction === 'Deposit') {
-    return balance + parseInt(values.value);
-  } else {
-    return balance - parseInt(values.value);
-  }
-};
