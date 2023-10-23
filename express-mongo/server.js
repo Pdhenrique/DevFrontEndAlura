c54c61d64bacb352b89d92ef1ad470c35d2092b8
Pedro Santana <pedroschw.c@gmail.com>
2023-10-23 12:36:17 -0300

    feat: add dotenv local variables

--- express-mongo/server.js
+++ express-mongo/server.js
@@ -1,7 +1,8 @@
+import "dotenv/config"
 import App from "./src/app.js"
 
 const PORT = 3000
 
 App.listen(PORT, () => {
-
+  console.log(`conectado a porta  ${PORT}`)
 })
--- express-mongo/src/config/dbConnect.js
+++ express-mongo/src/config/dbConnect.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
+  mongoose.connect(process.env.DB_CONNECTION_STRING)
 
-mongoose.connect('mongodb+srv://pedroschwc:8dXnBRapdr8ekY3a@cluster0.2wg0vok.mongodb.net/?retryWrites=true&w=majority')
-
-return mongoose.connection;
-
+  return mongoose.connection;
 }
 
 export default conectaNaDatabase;
